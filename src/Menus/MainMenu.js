026b9f5a7129fb7bb039d5b336c08378ed797c1e
Stefan <stefan.piatkov@gmail.com>
2017-06-02 16:32:36 +1000

    add navigation

---
+++ src/Menus/MainMenu.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MainMenu = () => {
+	return 
+
+	<h1>Main Menu</h1>
+
+	
+}
+
+export default MainMenu;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+// import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
+import NavComponent from './NavComponent/NavComponent.js'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<NavComponent />, document.getElementById('root'));
 registerServiceWorker();
