99c67da76741209fefa01dc65a331e0fffb8b4c5
Stefan <stefan.piatkov@gmail.com>
2017-06-02 16:35:08 +1000

    comment out part to allow compile

--- src/NavComponent/NavComponent.js
+++ src/NavComponent/NavComponent.js
@@ -53,13 +53,13 @@ import App from '../App.js'
 		    </div>
 		</Router>
 
-		burgerToggle() {
-			let linksEl = document.querySelector('.narrowLinks');
-			if (linksEl.style.display === 'block') {
-			    linksEl.style.display = 'none';
-			} else {
-			    linksEl.style.display = 'block';
-			}
+		// burgerToggle() {
+		// 	let linksEl = document.querySelector('.narrowLinks');
+		// 	if (linksEl.style.display === 'block') {
+		// 	    linksEl.style.display = 'none';
+		// 	} else {
+		// 	    linksEl.style.display = 'block';
+		// 	}
 	);
 // }; //End class
 
